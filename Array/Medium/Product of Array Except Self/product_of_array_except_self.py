f98faae0760bdddcd043d951ad49ef6a7caa1fe7
G-V-Krishna-Kumar <kalyangvkk2004@gmail.com>
2024-05-17 19:36:24 +0530

    Added product_of_array_except_self.py file

---
+++ Array/Medium/Product of Array Except Self/product_of_array_except_self.py
@@ -0,0 +1,20 @@
+class Solution:
+    def productExceptSelf(self, nums:list[int]) -> list[int]:
+        answer = [1] * len(nums)
+
+        prefix = 1
+        for i in range(len(nums)):
+            answer[i] = prefix
+            prefix *= nums[i]
+        
+        postfix = 1
+        for i in range(len(nums)-1, -1, -1):
+            answer[i] *= postfix
+            postfix *= nums[i]
+        
+        return answer
+
+solution_class = Solution()
+nums = eval(input("Enter array nums: "))
+answer = solution_class.productExceptSelf(nums)
+print(answer)

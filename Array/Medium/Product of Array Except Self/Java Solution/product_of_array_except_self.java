6b63a6dd0f5d8e1f6c8d3d94ebe7129ec0c37d2b
G-V-Krishna-Kumar <kalyangvkk2004@gmail.com>
2024-05-18 06:32:39 +0530

    Added product_of_array_except_self.java file

---
+++ Array/Medium/Product of Array Except Self/Java Solution/product_of_array_except_self.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class product_of_array_except_self {
+    public static void main(String[] args) {
+        Solution solution_class = new Solution();
+        Scanner scanner = new Scanner(System.in);
+        int n, i;
+        System.out.print("Enter length of array nums: ");
+        n = scanner.nextInt();
+
+        int[] nums = new int[n];
+        int[] answer = new int[n];
+
+        for (i=0; i<n; i++) {
+            System.out.print("Enter element nums[" + i + "]: ");
+            nums[i] = scanner.nextInt();
+        }
+
+        scanner.close();
+        answer = solution_class.prodExceptSelf(nums);
+        for (i=0; i<n; i++) {
+            System.out.print(answer[i] + " ");
+        }
+    }
+}
+
+class Solution {
+    public int[] prodExceptSelf (int[] nums) {
+        int n = nums.length;
+        int[] answer = new int[n];
+
+        int prefix = 1, i;
+        for (i=0; i<n; i++) {
+            answer[i] = prefix;
+            prefix *= nums[i];
+        }
+
+        int postfix=1;
+        for (i=n-1; i>=0; i--) {
+            answer[i] *= postfix;
+            postfix *= nums[i];
+        }
+
+        return answer;
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # LeetCode75
+
+### Solving LeetCode75 in Python, C++, Java
+### My goal is to learn new algorithms and update my programming skills
